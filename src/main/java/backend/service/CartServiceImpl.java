78cce4a7cf4a1d77159d3aa2833c4a6c959cc558
Erwin Lee <erwin@mokapos.com>
2020-04-12 14:43:28 +0800

    add unit test for cart

--- src/main/java/backend/service/CartServiceImpl.java
+++ src/main/java/backend/service/CartServiceImpl.java
@@ -1,10 +1,8 @@
 package backend.service;
 
 import java.math.BigDecimal;
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
